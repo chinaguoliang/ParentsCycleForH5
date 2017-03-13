814c38ff1b9dbb2112ca2e907358f9f74018e50e
chen <38033919@qq.com>
2017-03-13 12:00:34 +0800

    增加vux 控件

--- src/components/AnnouncementDetail.vue
+++ src/components/AnnouncementDetail.vue
@@ -1,26 +1,40 @@
 <template>
 
   <div class="hello">
-    <ol>
+    <!--<swiper :list="imgs" auto style="width:100%;margin:0 auto;" height="180px" dots-class="custom-bottom" dots-position="center"></swiper>-->
       <li v-for="item in datas">
         {{item.class_id}}
       </li>
-    </ol>
-
+    <group>
+      <cell title="title" value="value"></cell>
+    </group>
+    <rater></rater>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
+//import Swiper from 'vux';
+import { Group, Cell,Rater } from 'vux'
+
+
 export default {
   name: 'AnnouncementDetail',
   data () {
     return {
       msg: 'ParentsCycle',
-      datas:''
+      datas:'',
+      imgs:[
+     'http://file06.16sucai.com/2016/0222/17714c3b51079911760e5ef7fdb553f6.jpg',
+     'http://pic.58pic.com/58pic/15/67/98/93C58PICjeM_1024.jpg',
+     'http://file06.16sucai.com/2016/0315/1df566087c24a94cd9534bc9bc1871ff.jpg'
+     ],
     }
-  },
-  mounted() {
+  },components:{
+      Group,
+      Cell,
+      Rater
+  },mounted() {
       this.$http.jsonp('http://localhost:8081/videoTime/getVideoControlTime?schoolid=1&classid=')
         .then(function (response) {
           console.log(response.data)
